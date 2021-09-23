20083f554b601bb95b1514c49bb9942799c3004c
Mertt Doğanay <MERTLOL3@outlook.com>
2021-09-22 23:20:39 -0400

    Now you can add items with the return key instead if clicking the add button

--- app/src/main/java/com/example/shoppinglistappandroid/MainActivity.java
+++ app/src/main/java/com/example/shoppinglistappandroid/MainActivity.java
@@ -1,24 +1,31 @@
 package com.example.shoppinglistappandroid;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.SparseBooleanArray;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ArrayList<String> itemList;
     private ListView shopList;
     private EditText writeItem;
-    private Button button;
+//    private Button button;
     private ArrayAdapter<String> array;
 
     @Override
@@ -27,24 +34,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         shopList = (ListView) findViewById(R.id.list_view);
         writeItem = (EditText) findViewById(R.id.edit_text);
-        button = (Button) findViewById(R.id.add_Button);
+//        button = (Button) findViewById(R.id.add_Button);
 
+        itemList = new ArrayList<>();
 
         array = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
         shopList.setAdapter(array);
 
-        button.setOnClickListener(new View.OnClickListener() {
-
+        writeItem.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
-            public void onClick(View v) {
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_NULL)) {
+                    String item = writeItem.getText().toString();
+                    array.add(item);
+                    array.notifyDataSetChanged();
+                    writeItem.setText("");
 
-                String item = writeItem.getText().toString();
-                array.add(item);
-                array.notifyDataSetChanged();
-                writeItem.setText("");
+                }
+
+                return false;
             }
-
         });
+//        protected void deleteItem(TextView view){
+//
+//        }
+
     }
 }
 
